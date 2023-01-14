759cca0aae49f2029be64cc13800dfdde8c0da08
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-01-14 17:04:08 +0300

    Проверка на четность

---
+++ homework_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Проверка на четность!");
+
+Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+Console.WriteLine("True!!!");
+
+else
+Console.WriteLine("False!!!");
+
