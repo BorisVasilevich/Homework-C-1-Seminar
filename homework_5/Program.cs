debaf32f9b175c4d509305e17a91f1cb3541e0ee
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-01-14 17:16:17 +0300

    Диапазон четных чисел

---
+++ homework_5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Показ диапазона четных чисел от 1 до числа!");
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count < number)
+{
+    count+=2;
+    Console.Write(count+" ");
+}
+
+
+
