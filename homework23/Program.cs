8dbcdb8fcdd4bd61cc76cddef9d9a6941d0c49ce
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-01-23 20:07:24 +0300

    задача23

--- homework23/Program.cs
+++ homework23/Program.cs
@@ -19,3 +19,34 @@ Console.Write($"  {result}");
 count++;
 }
 
+
+
+
+int xa, ya, xb, yb;
+Console.WriteLine("Введите координату Х точки А: ");
+while (!int.TryParse(Console.ReadLine(), out xa))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Y точки А: ");
+while (!int.TryParse(Console.ReadLine(), out ya))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Х точки B: ");
+while (!int.TryParse(Console.ReadLine(), out xb))
+{
+Console.Write("Введен неверный параметр");
+}
+Console.WriteLine("Введите координату Х точки B: ");
+while (!int.TryParse(Console.ReadLine(), out yb))
+{
+Console.Write("Введен неверный параметр");
+}
+double Distance(double xa, double ya, double xb, double yb)
+{
+return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
+}
+
+double res = Distance(xa, ya, xb, yb);
+Console.WriteLine($"Расстоняние между двум точками - {res}");
