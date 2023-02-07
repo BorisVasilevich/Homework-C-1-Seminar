316407a0d7b7bcd037b9418f5265ce7548714fde
BorisVasilevich <121186717+BorisVasilevich@users.noreply.github.com>
2023-02-07 21:35:27 +0300

    задача52

---
+++ homework52/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int [,] GeneratMatrix(int m, int n, int min, int max)
+{
+    int [,] matrix = new int [m, n];
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0);i++)
+        for (int j = 0; j < matrix.GetLength(1);j++)
+        matrix [i,j] =  random.Next(min, max);
+    return matrix;
+}
+
+void PrintMatrix2D(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0);i++){
+        for (int j = 0; j < matrix.GetLength(1);j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.Write("  ");
+        Console.WriteLine();
+    }    
+}
+
+int [,] matrix = GeneratMatrix(3, 4, 0, 10);
+
+PrintMatrix2D(matrix);
+
+
+    double result = 0;
+    
+    for (int j = 0;j < matrix.GetLength(1);j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0);i++)
+        {
+            result += matrix [i,j]; 
+        }
+        Console.Write($" {Math.Round(result / matrix.GetLength(0), 1)}");
+        result = 0;
+    }
+
+
+
+
